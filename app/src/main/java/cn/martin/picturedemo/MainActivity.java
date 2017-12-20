1a4c0fe3a1c30332cc43ff69d03994d780b07187
MartinLi <383009029@qq.com>
2017-12-20 09:20:06 +0800

    Initial commit

---
+++ app/src/main/java/cn/martin/picturedemo/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.martin.picturedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author ex-lisuyang001
+ */
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
